bd9827ec4d27dd53ca20a7f52fd0f17182be5133
MiguelLouisLeRoux <miguellerouxleroux@gmail.com>
2021-08-30 11:44:18 +0200

    reconfigured obj file

--- GreetingsInCSharp.Test/KeepTrackOfNamesGreetedTest.cs
+++ GreetingsInCSharp.Test/KeepTrackOfNamesGreetedTest.cs
@@ -21,6 +21,7 @@ namespace GreetingsInCSharp.Models.Test
             greetings.GreetName();
             greetings.GetNameAndLanguage("Tess", "Japanese");
             greetings.GreetName();
+            Console.WriteLine(greetings.namesList.Count);
             
             Assert.Equal(new Dictionary<String, Int32>() {{"Pete", 1}, {"Jack", 1}, {"Tess", 1}}, greetings.namesList);
 
--- GreetingsInCSharpMVC/Program.cs
+++ GreetingsInCSharpMVC/Program.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace GreetingsInCSharp
 {
@@ -22,7 +16,7 @@ namespace GreetingsInCSharp
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://*:" + Environment.GetEnvironmentVariable("PORT"));
+                    // webBuilder.UseUrls("http://*:" + Environment.GetEnvironmentVariable("PORT"));
                 });
     }
 }
